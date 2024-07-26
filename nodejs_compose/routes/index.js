d2a4dfb56341d280952207845d7190d724412fac
Sheep1221 <kmkevin332907@gmail.com>
2024-07-27 01:21:25 +0800

    docker project initialize

---
+++ nodejs_compose/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/admin', function(req, res, next){
+  res.send('This is the page for admin');
+});
+
+module.exports = router;
